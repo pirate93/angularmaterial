7c995c8f8bb890a33bf915d2441262adbffca2a5
pirate93 <hemanth.bharan93@gmail.com>
2016-07-26 10:20:07 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-angular.module('materialApp',['ngMaterial', 'ngRoute'])
-	.config(['$routeProvider',
-	  	function($routeProvider) {
+angular.module('materialApp',['ngMaterial', 'ngRoute', 'ngMdIcons'])
+	.config(['$routeProvider', '$locationProvider',
+	  	function($routeProvider, $locationProvider) {
 	    	$routeProvider.
 	      	when('/', {
 	        	templateUrl: 'templates/mainPage.html',
@@ -9,6 +9,8 @@ angular.module('materialApp',['ngMaterial', 'ngRoute'])
 	      	otherwise({
 	        	redirectTo: '/'
 	    	});
+
+	    	$locationProvider.html5Mode(true);
 	  	}
   	])
   	.controller('MainController', function($scope, $http) {
@@ -17,11 +19,11 @@ angular.module('materialApp',['ngMaterial', 'ngRoute'])
 			method: 'GET',
 			url: 'http://api.themoviedb.org/3/movie/now_playing?api_key=dcb9184c0fa834e428249a41f467c635&page=1'
 		}).then(function successCallback(response) {
-		    $scope.movies = response.data.results;
+		    $scope.shows = response.data.results;
 		  }, function errorCallback(response) {
 		    console.log('no movies');
 		});
-		//Tv shows
+		/*//Tv shows
 		$http({
 			method: 'GET',
 			url: 'http://api.themoviedb.org/3/tv/airing_today?api_key=dcb9184c0fa834e428249a41f467c635&page=1'
@@ -38,10 +40,65 @@ angular.module('materialApp',['ngMaterial', 'ngRoute'])
 		    $scope.persons = response.data.results;
 		  }, function errorCallback(response) {
 		    console.log('no persons');
+		});*/
+
+		$scope.$on('show:select', function(event, data) {
+			$scope.select = data.select;
+			$scope.showBy = data.showBy;
+			if($scope.select=="person" && $scope.showBy=="top_rated"){
+				$scope.showBy = "latest";
+			}
+			$http({
+				method: 'GET',
+				url: 'http://api.themoviedb.org/3/'+$scope.select+'/'+$scope.showBy+'?api_key=dcb9184c0fa834e428249a41f467c635&page=1'
+			}).then(function successCallback(response) {
+			    $scope.shows = response.data.results;
+			  }, function errorCallback(response) {
+			    console.log('no tv shows');
+			});
 		});
   	})
 	.controller('AppCtrl', function($scope) {
 		
 	})
-	.controller('SideNavCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+	.controller('SideNavCtrl', function ($scope) {
+		$scope.movie = "buttonColorOnSelect";
+		$scope.tvShow = "";
+		$scope.person = "";
+		$scope.popular = "buttonColorOnSelect";
+		$scope.topRated = "";
+		$scope.select = "movie";
+		$scope.showBy = "popular";
+		$scope.btnSelected = function($event) {
+			$scope.select = $event.currentTarget.value;
+			$scope.movie = "";
+			$scope.tvShow = "";
+			$scope.person = "";
+			if($scope.select==="movie"){
+				$scope.movie = "buttonColorOnSelect";
+			}else if($scope.select==="tv"){
+				$scope.tvShow = "buttonColorOnSelect";
+			}else if($scope.select==="person"){
+				if($scope.showBy==="top_rated"){
+					$scope.topRated = "";
+					$scope.popular = "buttonColorOnSelect";
+					$scope.showBy = "popular";
+				}
+				$scope.person = "buttonColorOnSelect";
+			}
+			$scope.data = {'select': $scope.select, 'showBy': $scope.showBy};
+			$scope.$broadcast('show:select',$scope.data);
+		}
+		$scope.categorySelected = function($event) {
+			$scope.showBy = $event.currentTarget.value;
+			$scope.popular = "";
+			$scope.topRated = "";
+			if($scope.showBy==="popular"){
+				$scope.popular = "buttonColorOnSelect";
+			}else if($scope.showBy==="top_rated"){
+				$scope.topRated = "buttonColorOnSelect";
+			}
+			$scope.data = {'select': $scope.select, 'showBy': $scope.showBy};
+			$scope.$broadcast('show:select',$scope.data);
+		}
 	});
